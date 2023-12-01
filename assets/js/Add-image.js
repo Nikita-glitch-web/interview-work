97472aad6ae116342c3132400ae6faeef52c9a35
Nick <nikitalytvynov0506@gmail.com>
2023-12-02 01:11:29 +0200

    ещз

---
+++ assets/js/Add-image.js
@@ -0,0 +1,9 @@
+function save() {
+  let f = file1.files[0];
+  if (f) {
+    image1.src = URL.createObjectURL(f);
+    localStorage.setItem("myImage", image1.src);
+  }
+}
+
+image1.src = localStorage.getItem("myImage");
